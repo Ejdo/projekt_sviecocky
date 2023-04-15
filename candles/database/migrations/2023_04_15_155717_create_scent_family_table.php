cf3ad144becd6dbacab13fef5a50f5426d75defa
Matúš Ján Haľama <55839292+MatHal-STU@users.noreply.github.com>
2023-04-15 21:40:05 +0200

    scent_family

---
+++ candles/database/migrations/2023_04_15_155717_create_scent_family_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('scent_family', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('scent1_id');
+            $table->foreign('scent1_id')->references('id')->on('scent_type');
+            $table->bigInteger('scent2_id');
+            $table->foreign('scent2_id')->references('id')->on('scent_type');
+            $table->bigInteger('scent3_id');
+            $table->foreign('scent3_id')->references('id')->on('scent_type');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scent_family');
+    }
+};
